8748f0bd0b3fc5729b09e975c397687495c1a2fa
Daniil Koshelyuk <daniil.koshelyuk@gmail.com>
2023-03-27 18:01:37 +0200

    Add basic grid prototypes

---
+++ Runtime/Scripts/Grid/Grid.cs
@@ -0,0 +1,159 @@
+﻿#define DEBUG
+// #undef DEBUG
+// #define DEBUG2
+#undef DEBUG2
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+
+namespace PolytopeSolutions.Toolset.Grid {
+    public class GridElement {
+        public GridElement() {}
+    }
+    public abstract class Grid<T> where T : GridElement, new() {
+        protected List<T> _elements;
+        protected int[] _dimensions;
+
+        public T this[int i] {
+            get {
+                return this._elements[i];
+            }
+            set {
+                this._elements[i] = value;
+            }
+        }
+        public T this[int[] axisIndices] {
+            get {
+                return this[Index(axisIndices)];
+            }
+            set {
+                this[Index(axisIndices)] = value;
+            }
+        }
+        public int[] LastElementIndex {
+            get {
+                int[] lastElementIndex = new int[this._dimensions.Length];
+                for (int d = 0; d< this._dimensions.Length; d++) {
+                    lastElementIndex[d] = this._dimensions[d] - 1;
+                }
+                return lastElementIndex;
+            }
+        }
+        public int Count {
+            get {
+                if (this._dimensions == null || this._dimensions.Length == 0)
+                    return 0;
+                return Index(this.LastElementIndex)+1;
+            }
+        }
+        public int[] dimensions => this._dimensions;
+        public List<T> elements => this._elements;
+
+        public Grid() {
+            this._elements = new List<T>();
+        }
+        public Grid(int[] dimensions) : this() {
+            this._dimensions = dimensions;
+
+            int count = dimensions[0];
+            for (int d = 1; d < dimensions.Length; d++)
+                count *= dimensions[d];
+            for (int i = 0; i < count; i++)
+                this._elements.Add(null);
+        }
+        public virtual void Update(int[] dimensions) {
+            bool changed = (dimensions.Length != this._dimensions?.Length);
+            if (!changed) { 
+                for (int d = 0; d < dimensions.Length; d++) { 
+                    changed |= this._dimensions[d] != dimensions[d];
+                }
+            }
+            if (changed) {
+                int oldCount = this.Count;
+                this._dimensions = dimensions;
+                int newCount = this.Count;
+                if (oldCount < newCount) { 
+                    Insert(LastElementIndex, new T());
+                }
+                else if (oldCount > newCount) {
+                    for (int i = newCount+1; i < oldCount; i++) { 
+                        RemoveAt(i); 
+                    }
+                }
+            }
+        }
+        //////////////////////////////////////////////////////////////////////////////////
+        public virtual int Index(int[] axisIndices) {
+            int index = 0, count = 0;
+
+            for (int d0 = 1; d0 < this._dimensions.Length; d0++) {
+                count = this._dimensions[d0];
+                for (int d1 = d0+1; d1 < this._dimensions.Length; d1++){
+                    count *= this._dimensions[d1];
+                }
+
+                index += axisIndices[d0 - 1] * count;
+            }
+            index += axisIndices[axisIndices.Length - 1];
+            return index;
+        }
+        public virtual int[] AxisInidices(int index) { 
+            int[] axisIndices = new int[this._dimensions.Length];
+            int count = 0;
+
+            for (int d0 = 1; d0 < this._dimensions.Length; d0++) {
+                count = this._dimensions[d0];
+                for (int d1 = d0 + 1; d1 < this._dimensions.Length; d1++) {
+                    count *= this._dimensions[d1];
+                }
+
+                axisIndices[d0 - 1] = index/count;
+                index -= axisIndices[d0 - 1] * count;
+            }
+            axisIndices[this._dimensions.Length - 1] = index;
+
+            return axisIndices;
+        }
+        public virtual bool ValidIndex(int[] axisIndices) { 
+            if (this._dimensions == null || this._dimensions.Length == 0 || this._dimensions.Length != axisIndices.Length) return false;
+            bool isValid = false;
+            for (int d = 0; d < this._dimensions.Length; d++)
+                isValid |= ((this._dimensions[d] > axisIndices[d]) && (axisIndices[d] > 0));
+            return isValid;
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////
+        public virtual void Insert(int i, T element) {
+            while (this._elements.Count <= i)
+                this._elements.Add(new T());
+            this._elements[i] = element;
+        }
+        public virtual void Insert(int[] axisIndices, T element) {
+            Insert(Index(axisIndices), element);
+        }
+        public virtual void RemoveAt(int i) {
+            if (i < this.Count)
+                this._elements.RemoveAt(i);
+        }
+        public virtual void RemoveAt(int[] axisIndices) {
+            this._elements.RemoveAt(Index(axisIndices));
+        }
+    }
+    public static partial class ObjectHelpers {
+        public static int[] ToIntArray(this Vector3Int vector) { 
+            return new int[] { vector.x, vector.y, vector.z };
+        }
+        public static int[] ToIntArray(this Vector2Int vector) { 
+            return new int[] { vector.x, vector.y };
+        }
+        public static Vector3Int ToVector3Int(this int[] array) {
+            if (array.Length == 3) {
+                return new Vector3Int(array[0], array[1], array[2]);
+            }
+            return Vector3Int.zero;
+        }
+    }
+}
---
+++ Runtime/Scripts/Grid/GridElementConstraint.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using PolytopeSolutions.Toolset.GlobalTools.Types;
+using System.Linq;
+using System;
+
+namespace PolytopeSolutions.Toolset.Grid {
+    public interface GridElementConstraint {
+        public bool IsSatisfied(List<GridElementConstraint> otherConstraints);
+    }
+    [System.Serializable]
+    public class GridElementNeighborConstraint : GridElementConstraint { 
+        public enum ConnectorDirection { 
+            Up,
+            Down,
+            Front,
+            Back,
+            Left,
+            Right
+        }
+        public static Vector3Int NeighborOffset(GridElementNeighborConstraint constraint) {
+            int x = 0, y = 0, z = 0;
+            x = (constraint.direction == ConnectorDirection.Right) ? 1 : (constraint.direction == ConnectorDirection.Left) ? -1 : 0;
+            y = (constraint.direction == ConnectorDirection.Up) ? 1 : (constraint.direction == ConnectorDirection.Down) ? -1 : 0;
+            z = (constraint.direction == ConnectorDirection.Front) ? 1 : (constraint.direction == ConnectorDirection.Back) ? -1 : 0;
+            return new Vector3Int(x, y, z);
+        }
+        //[Flags]
+        public enum ConnectorType : UInt16 {
+            None,
+            Type0 = 1,
+            Type1 = 2,
+            Type2 = 4,
+            Type3 = 8,
+            Type4 = 16,
+            Type5 = 32,
+            Type6 = 64,
+            Type7 = 128,
+            Type8 = 256,
+            Type9 = 512
+        }
+        public ConnectorDirection direction;
+        public ConnectorType type;
+        public bool IsSatisfied(List<GridElementConstraint> otherConstraints) {
+            bool satisfied = false;
+            foreach (GridElementConstraint constraint in otherConstraints) { 
+                GridElementNeighborConstraint neighbor = (GridElementNeighborConstraint)constraint;
+                int currentDirection = (int)this.direction;
+                int neighborDirection = (int)neighbor.direction;
+                satisfied |= (((currentDirection % 2 == 0) && ((int)neighbor.direction % 2 == 1) && (neighborDirection - currentDirection == 1))
+                    || ((currentDirection % 2 == 1) && ((int)neighbor.direction % 2 == 0) && (neighborDirection - currentDirection == -1))
+                    && (this.type == neighbor.type));
+            }
+            return satisfied;
+        }
+    }
+}
