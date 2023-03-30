829b17cef9c0724e6f168c75bbdca6e2bd386ead
Daniil Koshelyuk <daniil.koshelyuk@gmail.com>
2023-03-30 18:37:20 +0200

    Add a consistent randomizer option to solver

--- Runtime/Scripts/Solvers/Solver.cs
+++ Runtime/Scripts/Solvers/Solver.cs
@@ -1,17 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 namespace PolytopeSolutions.Toolset.Solvers {
     public class Solver : MonoBehaviour {
         public bool flagForceResolve;
-        public bool flagAutoUpdate;
+        public bool flagAutoUpdateSolution;
+        public bool flagAutoUpdateSeed;
         protected bool flagSolutionChanged;
         protected bool flagSolutionUpdated;
+
+        public string seed;
+        protected System.Random randomizer;
         
         [ContextMenu("Solve")]
         public virtual void Solve() { }
         [ContextMenu("Clear")]
         public virtual void Clear() { }
+
+        protected void UpdateSeed() {
+            if (this.flagAutoUpdateSeed) { 
+                this.seed = DateTime.Now.ToString("yyyy-MM-dd-HH:mm:ss:fff");
+            }
+            this.randomizer = new System.Random(this.seed.GetHashCode());
+        }
     }
 }
