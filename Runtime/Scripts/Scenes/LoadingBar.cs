6a51b6a3b50cf0b57af934f5cdd9a11e3049e55d
Daniil Koshelyuk <daniil.koshelyuk@gmail.com>
2023-04-18 15:34:13 +0200

    Add simple object toggle utility

--- Runtime/Scripts/Scenes/LoadingBar.cs
+++ Runtime/Scripts/Scenes/LoadingBar.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using PolytopeSolutions.Toolset.GlobalTools.UI;
-using System.Runtime.CompilerServices;
 
 namespace PolytopeSolutions.Toolset.Scenes {
     public class LoadingBar : ProgressBar {
