5cb9bf8db93265d1ce7f89792ed81d47bd9c0bfe
Daniil Koshelyuk <daniil.koshelyuk@gmail.com>
2023-11-01 21:09:30 +0100

    unify first and third person camera controllers; introduce actions for third person controller to allow basic interactions

--- Runtime/Scripts/Input/FirstPersonCameraController.cs
+++ Runtime/Scripts/Input/FirstPersonCameraController.cs
@@ -7,57 +7,65 @@ using UnityEngine.InputSystem;
 namespace PolytopeSolutions.Toolset.Input {
 	[RequireComponent(typeof(Rigidbody))]
 	public class FirstPersonCameraController : MonoBehaviour {
+		[Header("Events")]
 		[SerializeField] private InputActionReference cameraRotateLeftRight;
 		[SerializeField] private InputActionReference cameraRotateUpDown;
-        [SerializeField] private InputActionReference cameraMoveLeftRight;
-        [SerializeField] private InputActionReference cameraMoveUpDown;
-        [SerializeField] private InputActionReference cameraMoveForwardBackward;
+		[SerializeField] private InputActionReference cameraMoveLeftRight;
+		[SerializeField] private InputActionReference cameraMoveUpDown;
+		[SerializeField] private InputActionReference cameraMoveForwardBackward;
 
+		[Header("General")]
 		[SerializeField] private float rotateSpeed = 25f;
 		[SerializeField] private float moveSpeed = 2f;
 
-		[SerializeField] private Camera viewerCamera;
+		[SerializeField] private Transform tCamera;
 		private new Rigidbody rigidbody;
+		private Camera cCamera;
 
 		private float rotateLeftRightValue;
-        private float rotateUpDownValue;
-        private float moveLeftRightValue;
-        private float moveUpDownValue;
-        private float moveForwardBackwardValue;
+		private float rotateUpDownValue;
+		private float moveLeftRightValue;
+		private float moveUpDownValue;
+		private float moveForwardBackwardValue;
 
-        private void OnEnable() {
+		private void OnEnable() {
 			this.rigidbody = gameObject.GetComponent<Rigidbody>();
+			this.cCamera = this.tCamera?.GetComponent<Camera>();
+			if (this.tCamera == null){
+				this.cCamera = Camera.main;
+				this.tCamera = this.cCamera?.transform;
+			}
 
-            this.cameraRotateLeftRight.action.Enable();
+			this.cameraRotateLeftRight.action.Enable();
 			this.cameraRotateUpDown.action.Enable();
 			this.cameraMoveLeftRight.action.Enable();
-            this.cameraMoveUpDown.action.Enable();
+			this.cameraMoveUpDown.action.Enable();
 			this.cameraMoveForwardBackward.action.Enable();
 
-            this.cameraRotateLeftRight.action.started += RotateLeftRightStarted;
-            this.cameraRotateUpDown.action.started += RotateUpDownStarted;
+			this.cameraRotateLeftRight.action.started += RotateLeftRightStarted;
+			this.cameraRotateUpDown.action.started += RotateUpDownStarted;
 			this.cameraMoveLeftRight.action.started += MoveLeftRightStarted;
-            this.cameraMoveUpDown.action.started += MoveUpDownStarted;
-            this.cameraMoveForwardBackward.action.started += MoveForwardBackwardStarted;
-            this.cameraRotateLeftRight.action.canceled += RotateLeftRightEnded;
-            this.cameraRotateUpDown.action.canceled += RotateUpDownEnded;
-            this.cameraMoveLeftRight.action.canceled += MoveLeftRightEnded;
-            this.cameraMoveUpDown.action.canceled += MoveUpDownEnded;
-            this.cameraMoveForwardBackward.action.canceled += MoveForwardBackwardEnded;
-        }
+			this.cameraMoveUpDown.action.started += MoveUpDownStarted;
+			this.cameraMoveForwardBackward.action.started += MoveForwardBackwardStarted;
+			this.cameraRotateLeftRight.action.canceled += RotateLeftRightEnded;
+			this.cameraRotateUpDown.action.canceled += RotateUpDownEnded;
+			this.cameraMoveLeftRight.action.canceled += MoveLeftRightEnded;
+			this.cameraMoveUpDown.action.canceled += MoveUpDownEnded;
+			this.cameraMoveForwardBackward.action.canceled += MoveForwardBackwardEnded;
+		}
 		private void OnDisable() {
-            this.cameraRotateLeftRight.action.started -= RotateLeftRightStarted;
-            this.cameraRotateUpDown.action.started -= RotateUpDownStarted;
-            this.cameraMoveLeftRight.action.started += MoveLeftRightStarted;
-            this.cameraMoveUpDown.action.started -= MoveUpDownStarted;
-            this.cameraMoveForwardBackward.action.started -= MoveForwardBackwardStarted;
-            this.cameraRotateLeftRight.action.canceled -= RotateLeftRightEnded;
-            this.cameraRotateUpDown.action.canceled -= RotateUpDownEnded;
-            this.cameraMoveLeftRight.action.canceled += MoveLeftRightEnded;
-            this.cameraMoveUpDown.action.canceled -= MoveUpDownEnded;
-            this.cameraMoveForwardBackward.action.canceled -= MoveForwardBackwardEnded;
+			this.cameraRotateLeftRight.action.started -= RotateLeftRightStarted;
+			this.cameraRotateUpDown.action.started -= RotateUpDownStarted;
+			this.cameraMoveLeftRight.action.started += MoveLeftRightStarted;
+			this.cameraMoveUpDown.action.started -= MoveUpDownStarted;
+			this.cameraMoveForwardBackward.action.started -= MoveForwardBackwardStarted;
+			this.cameraRotateLeftRight.action.canceled -= RotateLeftRightEnded;
+			this.cameraRotateUpDown.action.canceled -= RotateUpDownEnded;
+			this.cameraMoveLeftRight.action.canceled += MoveLeftRightEnded;
+			this.cameraMoveUpDown.action.canceled -= MoveUpDownEnded;
+			this.cameraMoveForwardBackward.action.canceled -= MoveForwardBackwardEnded;
 
-            this.cameraRotateLeftRight.action.Disable();
+			this.cameraRotateLeftRight.action.Disable();
 			this.cameraRotateUpDown.action.Disable();
 			this.cameraMoveUpDown.action.Disable();
 			this.cameraMoveForwardBackward.action.Disable();
@@ -70,10 +78,10 @@ namespace PolytopeSolutions.Toolset.Input {
 		}
 		private void RotateUpDownStarted(InputAction.CallbackContext context) { 
 			this.rotateUpDownValue = context.ReadValue<float>() * this.rotateSpeed;
-        }
+		}
 		private void RotateUpDownEnded(InputAction.CallbackContext context) { 
 			this.rotateUpDownValue = 0f;
-        }
+		}
 		private void MoveLeftRightStarted(InputAction.CallbackContext context) { 
 			this.moveLeftRightValue = context.ReadValue<float>() * this.moveSpeed;
 		}
@@ -87,30 +95,30 @@ namespace PolytopeSolutions.Toolset.Input {
 			this.moveUpDownValue = 0f;
 		}
 		private void MoveForwardBackwardStarted(InputAction.CallbackContext context) {
-            this.moveForwardBackwardValue = context.ReadValue<float>() * this.moveSpeed;
-        }
+			this.moveForwardBackwardValue = context.ReadValue<float>() * this.moveSpeed;
+		}
 		private void MoveForwardBackwardEnded(InputAction.CallbackContext context) {
-            this.moveForwardBackwardValue = 0f;
+			this.moveForwardBackwardValue = 0f;
 		}
 
 		void FixedUpdate() {
-			this.viewerCamera.transform.RotateAround(
-				this.viewerCamera.transform.position,
+			this.tCamera.RotateAround(
+				this.tCamera.position,
 				Vector3.up, 
 				this.rotateLeftRightValue * Time.fixedDeltaTime
 			);
-            this.viewerCamera.transform.RotateAround(
-				this.viewerCamera.transform.position,
-                this.viewerCamera.transform.right, 
+			this.tCamera.RotateAround(
+				this.tCamera.position,
+				this.tCamera.right, 
 				this.rotateUpDownValue * Time.fixedDeltaTime
 			);
 			Vector3 direction =
-                - this.viewerCamera.transform.right * this.moveLeftRightValue +
-                Vector3.up * this.moveUpDownValue +
-                this.viewerCamera.transform.forward * this.moveForwardBackwardValue;
+				- this.tCamera.right * this.moveLeftRightValue +
+				Vector3.up * this.moveUpDownValue +
+				this.tCamera.forward * this.moveForwardBackwardValue;
 			this.rigidbody.MovePosition(
 				transform.position + direction * Time.fixedDeltaTime
 			);
-        }
+		}
 	}
 }
--- Runtime/Scripts/Input/ThirdPersonCameraController.cs
+++ Runtime/Scripts/Input/ThirdPersonCameraController.cs
@@ -2,26 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.InputSystem;
+using static PolytopeSolutions.Toolset.GlobalTools.Generic.ObjectHelpers;
+
 namespace PolytopeSolutions.Toolset.Input {
+	[RequireComponent(typeof(Rigidbody))]
     public class ThirdPersonCameraController : MonoBehaviour {
+        [Header("Events")]
+        [SerializeField] private InputActionReference cameraRotateLeftRight;
+        [SerializeField] private InputActionReference cameraRotateUpDown;
+        [SerializeField] private InputActionReference cameraMoveLeftRight;
+        [SerializeField] private InputActionReference cameraMoveForwardBackward;
+        [SerializeField] private InputActionReference cameraMoveZoomInOut;
+
+        [Header("General")]
         [SerializeField] private Transform tCamera;
         [SerializeField] private Transform tTarget;
         [SerializeField] private Vector2 distanceRange;
         [SerializeField] private Vector2 verticalAngleRange;
-        [SerializeField] private Vector2 horizontalAngleRange;
+        //[SerializeField] private Vector2 horizontalAngleRange;
+
+        [SerializeField] private float rotateSpeed = 25f;
+        [SerializeField] private float moveSpeed = 2f;
+
+
+        private new Rigidbody rigidbody;
         private Camera cCamera;
         private float farCornerDistanceCache;
 
         public delegate void OnCameraViewChanged();
-        public OnCameraViewChanged onCameraViewChanged;
+        public OnCameraViewChanged onCameraViewChanged = null;
+
+        private float rotateLeftRightValue;
+        private float rotateUpDownValue;
+        private float moveLeftRightValue;
+        private float moveForwardBackwardValue;
+        private float moveZoomInOutValue;
 
         ///////////////////////////////////////////////////////////////////////
         private void OnEnable() {
+            this.rigidbody = gameObject.GetComponent<Rigidbody>();
             this.cCamera = this.tCamera?.GetComponent<Camera>();
             if (this.tCamera == null){
                 this.cCamera = Camera.main;
-                this.tCamera = this.cCamera.transform;
+                this.tCamera = this.cCamera?.transform;
             }
+
             float frustumHeight = 2.0f * this.cCamera.farClipPlane * Mathf.Tan(this.cCamera.fieldOfView * 0.5f * Mathf.Deg2Rad);
             float frustumWidth = frustumHeight * this.cCamera.aspect;
             this.farCornerDistanceCache = Mathf.Sqrt(
@@ -29,8 +55,76 @@ namespace PolytopeSolutions.Toolset.Input {
                 Mathf.Pow(frustumHeight / 2, 2) +
                 Mathf.Pow(frustumWidth / 2, 2)
             );
+
+            this.cameraRotateLeftRight.action.Enable();
+            this.cameraRotateUpDown.action.Enable();
+            this.cameraMoveLeftRight.action.Enable();
+            this.cameraMoveForwardBackward.action.Enable();
+            this.cameraMoveZoomInOut.action.Enable();
+
+            this.cameraRotateLeftRight.action.started += RotateLeftRightStarted;
+            this.cameraRotateUpDown.action.started += RotateUpDownStarted;
+            this.cameraMoveLeftRight.action.started += MoveLeftRightStarted;
+            this.cameraMoveForwardBackward.action.started += MoveForwardBackwardStarted;
+            this.cameraMoveZoomInOut.action.started += MoveZoomInOutStarted;
+            this.cameraRotateLeftRight.action.canceled += RotateLeftRightEnded;
+            this.cameraRotateUpDown.action.canceled += RotateUpDownEnded;
+            this.cameraMoveLeftRight.action.canceled += MoveLeftRightEnded;
+            this.cameraMoveForwardBackward.action.canceled += MoveForwardBackwardEnded;
+            this.cameraMoveZoomInOut.action.canceled += MoveZoomInOutEnded;
         }
-        private void Update() {
+        private void OnDisable() { 
+            this.cameraRotateLeftRight.action.Disable();
+            this.cameraRotateUpDown.action.Disable();
+            this.cameraMoveLeftRight.action.Disable();
+            this.cameraMoveForwardBackward.action.Disable();
+            this.cameraMoveZoomInOut.action.Disable();
+
+            this.cameraRotateLeftRight.action.started -= RotateLeftRightStarted;
+            this.cameraRotateUpDown.action.started -= RotateUpDownStarted;
+            this.cameraMoveLeftRight.action.started -= MoveLeftRightStarted;
+            this.cameraMoveForwardBackward.action.started -= MoveForwardBackwardStarted;
+            this.cameraMoveZoomInOut.action.started -= MoveZoomInOutStarted;
+            this.cameraRotateLeftRight.action.canceled -= RotateLeftRightEnded;
+            this.cameraRotateUpDown.action.canceled -= RotateUpDownEnded;
+            this.cameraMoveLeftRight.action.canceled -= MoveLeftRightEnded;
+            this.cameraMoveForwardBackward.action.canceled -= MoveForwardBackwardEnded;
+            this.cameraMoveZoomInOut.action.canceled -= MoveZoomInOutEnded;
+        }
+        private void FixedUpdate() {
+            Vector3 currentPosition = this.tCamera.position;
+            Quaternion currentRotation = this.tCamera.rotation;
+            HandleInputs();
+            ConstrainCameraToTraget();
+            if (currentPosition != this.tCamera.position || currentRotation != this.tCamera.rotation)
+                this.onCameraViewChanged?.Invoke();
+        }
+        private void OnDrawGizmos() {
+            if (this.tCamera == null || this.tTarget == null) return;
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(this.tCamera.position, this.tTarget.position);
+        }
+        ///////////////////////////////////////////////////////////////////////
+        private void HandleInputs() { 
+			this.tCamera.RotateAround(
+				this.tTarget.position,
+				Vector3.up, 
+				this.rotateLeftRightValue * Time.fixedDeltaTime
+			);
+            this.tCamera.transform.RotateAround(
+                this.tTarget.position,
+                this.tCamera.right,
+                this.rotateUpDownValue * Time.fixedDeltaTime
+            );
+            Vector3 direction =
+                -this.tCamera.right * this.moveLeftRightValue +
+                this.tCamera.forward.XZ().ToXZ() * this.moveForwardBackwardValue +
+                this.tCamera.forward * this.moveZoomInOutValue;
+            this.rigidbody.MovePosition(
+                transform.position + direction * Time.fixedDeltaTime
+            );
+        }
+        private void ConstrainCameraToTraget() {
             Vector3 lookDirection = this.tTarget.position - this.tCamera.position;
             float distance = lookDirection.magnitude;
             // Ensure camera within distance range
@@ -42,18 +136,40 @@ namespace PolytopeSolutions.Toolset.Input {
             verticalAngle = Mathf.Clamp(verticalAngle, this.verticalAngleRange.x, this.verticalAngleRange.y);
             lookDirection = Quaternion.AngleAxis(-verticalAngle, inPlaneNormal) * horizontalDirection.normalized;
             // Update the position and rotation of the camera
-            Vector3 currentPosition = this.tCamera.position;
-            Quaternion currentRotation = this.tCamera.rotation;
             this.tCamera.position = this.tTarget.position - lookDirection * distance;
             this.tCamera.LookAt(this.tTarget);
-            if (this.onCameraViewChanged != null
-                && currentPosition != this.tCamera.position || currentRotation != this.tCamera.rotation)
-                this.onCameraViewChanged.Invoke();
         }
-        private void OnDrawGizmos() {
-            if (this.tCamera == null || this.tTarget == null) return;
-            Gizmos.color = Color.red;
-            Gizmos.DrawLine(this.tCamera.position, this.tTarget.position);
+        ///////////////////////////////////////////////////////////////////////
+        private void MoveLeftRightStarted(InputAction.CallbackContext context) {
+            this.moveLeftRightValue = context.ReadValue<float>() * this.moveSpeed;
+        }
+        private void MoveLeftRightEnded(InputAction.CallbackContext context) {
+            this.moveLeftRightValue = 0f;
+        }
+        private void MoveForwardBackwardStarted(InputAction.CallbackContext context) {
+            this.moveForwardBackwardValue = context.ReadValue<float>() * this.moveSpeed;
+        }
+        private void MoveForwardBackwardEnded(InputAction.CallbackContext context){
+            this.moveForwardBackwardValue = 0f;
+        }
+        private void MoveZoomInOutStarted(InputAction.CallbackContext context) {
+            this.moveZoomInOutValue = context.ReadValue<float>() * this.moveSpeed;
+        }
+        private void MoveZoomInOutEnded(InputAction.CallbackContext context) {
+            this.moveZoomInOutValue = 0f;
+        }
+        private void RotateLeftRightStarted(InputAction.CallbackContext context)
+        {
+            this.rotateLeftRightValue = context.ReadValue<float>() * this.rotateSpeed;
+        }
+        private void RotateLeftRightEnded(InputAction.CallbackContext context) {
+            this.rotateLeftRightValue = 0f;
+        }
+        private void RotateUpDownStarted(InputAction.CallbackContext context) {
+            this.rotateUpDownValue = context.ReadValue<float>() * this.rotateSpeed;
+        }
+        private void RotateUpDownEnded(InputAction.CallbackContext context) {
+            this.rotateUpDownValue = 0f;
         }
         ///////////////////////////////////////////////////////////////////////
         public List<Vector3> FieldOfViewBoundaries(Plane plane) { 
