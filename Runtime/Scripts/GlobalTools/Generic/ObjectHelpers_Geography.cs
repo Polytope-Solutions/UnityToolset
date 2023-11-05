69f55359a48e7af2ae25c0a56eae4b2cc041dc63
Daniil Koshelyuk <daniil.koshelyuk@gmail.com>
2023-11-05 19:58:51 +0100

    try expand geo functionality

--- Runtime/Scripts/GlobalTools/Generic/ObjectHelpers_Geography.cs
+++ Runtime/Scripts/GlobalTools/Generic/ObjectHelpers_Geography.cs
@@ -10,6 +10,8 @@ namespace PolytopeSolutions.Toolset.GlobalTools.Generic {
             public delegate float TileSize(int zoomLevel);
             public static readonly float EarthCircumference = 2 * 6378137.0f * Mathf.PI;
             public static readonly float EarthRadius = 6372.7982f;
+            private static float TileSizeMeters(int zoomLevel) => Geography.EarthCircumference / Mathf.Pow(2, (float)zoomLevel);
+            private static float TileSizeDegrees(int zoomLevel) => 360f / Mathf.Pow(2, (float)zoomLevel);
             public static Vector2Int Geo2GeoTile(int zoomLevel, float lon, float lat) {
                 Vector2Int p = new Vector2Int(
                     Mathf.RoundToInt((float)((lon + 180.0) / 360.0 * (1 << zoomLevel))),
@@ -32,7 +34,7 @@ namespace PolytopeSolutions.Toolset.GlobalTools.Generic {
                     baseGeoCoordinate.x,
                     baseGeoCoordinate.y);
                 geoTileIndex.x += gameTileIndex.x;
-                geoTileIndex.y += gameTileIndex.y;
+                geoTileIndex.y -= gameTileIndex.y;
                 return geoTileIndex;
             }
             public static Vector2 GameTile2Geo(int zoomLevel, Vector2Int gameTileIndex, Vector2 baseGeoCoordinate) {
@@ -51,15 +53,36 @@ namespace PolytopeSolutions.Toolset.GlobalTools.Generic {
                 return worldCoordinate;
             }
             public static Vector2Int World2GameTile(int zoomLevel, Vector3 worldCoordinate, TileSize tileSize) {
+                float zoomTileSize = tileSize(zoomLevel);
                 Vector2Int gameTileIndex = new Vector2Int(
-                    Mathf.RoundToInt(worldCoordinate.x / tileSize(zoomLevel)),
-                    Mathf.RoundToInt(worldCoordinate.z / tileSize(zoomLevel))
+                    Mathf.RoundToInt(worldCoordinate.x / zoomTileSize),
+                    Mathf.RoundToInt(worldCoordinate.z / zoomTileSize)
                 );
                 return gameTileIndex;
             }
             public static Vector2Int World2GeoTile(int zoomLevel, Vector3 worldCoordinate, Vector2 baseGeoCoordinate, TileSize tileSize) {
-                Vector2Int geoTileIndex = GameTile2GeoTile(zoomLevel, World2GameTile(zoomLevel, worldCoordinate, tileSize), baseGeoCoordinate);
+                Vector2Int geoTileIndex = GameTile2GeoTile(zoomLevel, 
+                    World2GameTile(zoomLevel, worldCoordinate, tileSize), baseGeoCoordinate);
                 return geoTileIndex;
+            }
+            public static Vector2 World2Geo(int zoomLevel, Vector3 worldCoordinate, Vector3 baseGeoCoordinate, TileSize tileSize) {
+                float zoomTileSize = tileSize(zoomLevel);
+                Vector2 gameTileFractional = new Vector2(
+                    worldCoordinate.x / zoomTileSize,
+                    worldCoordinate.z / zoomTileSize
+                );
+                Vector2Int gameTileIndex = new Vector2Int(
+                    Mathf.RoundToInt(gameTileFractional.x),
+                    Mathf.RoundToInt(gameTileFractional.y)
+                );
+                gameTileFractional = new Vector2(
+                    gameTileFractional.x - (float)gameTileIndex.x,
+                    gameTileFractional.y - (float)gameTileIndex.y
+                );
+                Vector2 geoCoordinate = GameTile2Geo(zoomLevel, gameTileIndex, baseGeoCoordinate);
+                geoCoordinate.x += gameTileFractional.x * TileSizeDegrees(zoomLevel);
+                geoCoordinate.y += gameTileFractional.y * TileSizeDegrees(zoomLevel);
+                return geoCoordinate;
             }
         }
     }
